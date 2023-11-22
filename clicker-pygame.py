bff615d57b7807a344303bc893c8ac275c273bfb
Vojta <vojta.karasek1@gmail.com>
2023-11-22 21:10:04 +0100

    Screen initialization

--- clicker-pygame.py
+++ clicker-pygame.py
@@ -1,1 +1,13 @@
 import pygame
+
+pygame.init()
+size = pygame.display.get_desktop_sizes()
+screen = pygame.display.set_mode((size[0][0], size[0][1] - 55), pygame.RESIZABLE)
+
+pygame.display.flip()
+mainloop = True
+
+while mainloop:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            mainloop = False
