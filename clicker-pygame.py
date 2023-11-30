76a7db989c80e0be9a212119972781bdc9bf5f5b
RobinSvagr <130986777+RobinSvagr@users.noreply.github.com>
2023-11-30 14:36:16 +0100

    money text window

--- clicker-pygame.py
+++ clicker-pygame.py
@@ -1,8 +1,12 @@
+import index
 import pygame
 
 pygame.init()
 size = pygame.display.get_desktop_sizes()
 screen = pygame.display.set_mode((size[0][0], size[0][1] - 55), pygame.RESIZABLE)
+font = pygame.font.SysFont("arial", 36)
+text = font.render(index.casino_money.value_money(), True, (0, 0, 0))
+text_rect = text.get_rect(center=(size[0][0] // 2, (size[0][1] - 55) // 2))
 
 pygame.display.flip()
 mainloop = True
@@ -11,3 +15,7 @@ while mainloop:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             mainloop = False
+
+    screen.fill((255, 255, 255))
+    screen.blit(text, text_rect)
+    pygame.display.flip()
